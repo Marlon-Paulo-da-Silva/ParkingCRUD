5197b3ec520334dec7b9e0d11f573989dac983d6
Marlon-Paulo-da-Silva <marlon.paulo.silva@outlook.com>
2020-02-08 23:56:48 -0300

    criado CRUD dos controllers

--- backend/bin/express.js
+++ backend/bin/express.js
@@ -3,4 +3,9 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect(
+  "mongodb+srv://admin:abc1234@cluster0-hdgyo.mongodb.net/test?retryWrites=true&w=majority",
+  { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const app = require("./bin/express");
-
+const Parking = require("./module/parking/schema");
+const moment = require("moment");
 const port = 3333;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Ola marlon");
+app.get("/", async (req, res) => {
+  const pk = await Parking.create({
+    vehicle: "gol branco",
+    vehiclePlate: "CDF-4074",
+    parkingStartAt: moment(),
+    pricePerHour: 10
+  });
+
+  const all = await Parking.find();
+
+  res.status(200).send(all);
 });
 
 app.listen(port, () => {
