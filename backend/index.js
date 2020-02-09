8ffff6dc6bb481b25de7d7b97affdb0061bcd7a6
Marlon-Paulo-da-Silva <marlon.paulo.silva@outlook.com>
2020-02-09 00:12:10 -0300

    criado as rotas da CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,11 @@
 const app = require("./bin/express");
+const bodyParser = require("body-parser");
 const Parking = require("./module/parking/schema");
-const moment = require("moment");
+const routerParking = require("./module/parking/routes");
 const port = 3333;
 
-app.get("/", async (req, res) => {
-  const pk = await Parking.create({
-    vehicle: "gol branco",
-    vehiclePlate: "CDF-4074",
-    parkingStartAt: moment(),
-    pricePerHour: 10
-  });
-
-  const all = await Parking.find();
-
-  res.status(200).send(all);
-});
+app.use(bodyParser.json());
+app.use("/api/parking", routerParking);
 
 app.listen(port, () => {
   console.log(`Api initialized success in port: ${port}`);
